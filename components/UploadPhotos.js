72375c36f8d072f0aef8a3702ba232e0c62b6d03
Big One <sponjjanc@naver.com>
2023-03-27 22:35:28 +0900

    Feat: 상품 사진 업로드

---
+++ components/UploadPhotos.js
@@ -0,0 +1,77 @@
+import React, {useCallback, useState} from "react";
+import {Modal, Upload} from "antd";
+import {PlusOutlined} from "@ant-design/icons";
+
+const getBase64 = (file) =>
+    new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = (error) => reject(error);
+    });
+
+const UploadPhotos = () => {
+    const [previewOpen, setPreviewOpen] = useState(false);
+    const [previewImage, setPreviewImage] = useState('');
+    const [previewTitle, setPreviewTitle] = useState('');
+    const [fileList, setFileList] = useState([]);
+
+    if (fileList.length < 1) {
+        setFileList((prev) => prev.concat({
+            uid: '-1',
+            name: 'image.png',
+            status: 'done',
+            url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+        },));
+    }
+    console.log(fileList.length);
+    const handleChange = ({ fileList: newFileList }) => setFileList(newFileList);
+    const handleCancel = () => setPreviewOpen(false);
+    const handlePreview = async (file) => {
+        if (!file.url && !file.preview) {
+            file.preview = await getBase64(file.originFileObj);
+        }
+        setPreviewImage(file.url || file.preview);
+        setPreviewOpen(true);
+        setPreviewTitle(file.name || file.url.substring(file.url.lastIndexOf('/') + 1));
+    };
+    const uploadButton = useCallback(() => {
+        return (
+            <div>
+                <PlusOutlined/>
+                <div
+                    style={{
+                        marginTop: 8,
+                    }}
+                >
+                    Upload
+                </div>
+            </div>
+        );
+    }, []);
+    return(
+        <>
+            {/*<span>사진</span>*/}
+            <Upload
+                action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                listType="picture-card"
+                fileList={fileList}
+                onPreview={handlePreview}
+                onChange={handleChange}
+            >
+                {fileList.length >= 8 ? null : uploadButton()}
+            </Upload>
+            <Modal open={previewOpen} title={previewTitle} footer={null} onCancel={handleCancel}>
+                <img
+                    alt="example"
+                    style={{
+                        width: '100%',
+                    }}
+                    src={previewImage}
+                />
+            </Modal>
+        </>
+    )
+}
+
+export default UploadPhotos;
